43eac4a12609778032a0be549e011b937cb0e03d
unknown <olixis@gmail.com>
2017-06-28 19:18:00 -0300

    form de email

--- js/contact_me.js
+++ js/contact_me.js
@@ -27,7 +27,7 @@ $(function() {
                 'Authorization': 'Basic ' + window.btoa('api:key-0b6d1e7915f49980434aa50e15b75e3b'),
 				'Access-Control-Allow-Origin': '*',
 				'Access-Control-Allow-Methods': 'OPTIONS,POST',
-				'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers,Origin, X-Requested-With, Content-Type, Accept, Authorization'
+				'Access-Control-Allow-Headers': 'Access-Control-Allow-Headers,Access-Control-Allow-Origin,Origin,Access-Control-Allow-Methods, X-Requested-With, Content-Type, Accept, Authorization'
                 },
 				crossDomain: true,
                 cache: false,
